cd9c32087a9283e90a55a84d9529a092bb9c3ee3
0xFadedd <104228340+0xFadedd@users.noreply.github.com>
2022-04-22 21:15:00 +0100

    Add files via upload

---
+++ ERC721Z.sol
@@ -0,0 +1,205 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract ERC721Z {
+
+    //project context
+    string public name;
+    string public symbol;
+    string internal baseTokenURI;
+    string internal baseTokenExtension;
+    uint256 public totalSupply;
+
+    constructor (
+        string memory name_,
+        string memory symbol_
+    ) {
+        name = name_;
+        symbol = symbol_;
+    }
+
+    //Standard ERC721 Events
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+    
+    //Mappings
+    mapping(uint256 => address) public ownerOf;
+    mapping(address => uint256) public balanceOf;
+    mapping(uint256 => address) public isApproved;
+    mapping(address => mapping(address => bool)) public isApprovedForAll;
+
+
+    //////Internal Write Functions
+    ////Mint Functions
+    //mint function
+    function _mint(address to_, uint256 tokenId_) internal {
+        require(to_ != address(0x0));
+        require(ownerOf[tokenId_] == address(0x0));
+
+        ownerOf[tokenId_] = to_;
+        balanceOf[to_]++;
+        totalSupply++;
+        emit Transfer(address(0x0), to_, tokenId_);
+    }
+
+    //transfer function
+    function _transfer(address from_, address to_, uint256 tokenId_) internal virtual {
+        require(ownerOf[tokenId_] == from_);
+        require(to_ != address(0x0));
+        
+        ownerOf[tokenId_] = to_;
+        balanceOf[from_]--;
+        balanceOf[to_]++;
+    }
+
+    ////Internal View functions
+    //Is approved or the owner of the token
+    function _isApprovedOrOwner(address spender_, uint256 tokenId_) internal view virtual returns (bool) {
+        require(ownerOf[tokenId_] != address(0x0));
+        address owner_ = ownerOf[tokenId_];
+        return (spender_ == owner_ || spender_ == isApproved[tokenId_] || isApprovedForAll[owner_][spender_]);
+    }
+    //Checks to see if a token exists
+    function _exists(uint256 tokenId_) internal view virtual returns (bool) {
+        return ownerOf[tokenId_] != address(0x0);
+    }
+
+    //OZ standard function to convert Uint to String
+    function _NumToString(uint256 value_) internal pure returns (string memory) {
+        if (value_ == 0) { return "0"; }
+        uint256 _iterate = value_;
+        uint256 _digits;
+        while (_iterate != 0) {
+            _digits++;
+            _iterate /= 10;
+        }
+        bytes memory _buffer = new bytes(_digits);
+        while (value_ != 0) {
+            _digits--;
+            _buffer[_digits] = bytes1(uint8(48 + uint256(value_ % 10)));
+            value_ /= 10;
+        }
+        return string(_buffer);
+    }
+
+
+    ////Approve functions
+    //approve
+    function _approve(address to_, uint256 tokenId_) internal virtual {
+        if (isApproved[tokenId_] != to_) {
+            isApproved[tokenId_] = to_;
+            emit Approval(ownerOf[tokenId_], to_, tokenId_);
+        }
+    }
+    //approve all
+    function _setApprovalForAll(address owner_, address operator_, bool approved_) internal virtual {
+        require(owner_ != operator_);
+        isApprovedForAll[owner_][operator_] = approved_;
+        emit ApprovalForAll(owner_, operator_, approved_);
+    }
+
+    ////Public Write Functions
+
+    function approve(address to_, uint256 tokenId_) public virtual {
+        address _owner = ownerOf[tokenId_];
+        require(to_ != _owner);
+        require(msg.sender == _owner || isApprovedForAll[_owner][msg.sender]);
+        _approve(to_, tokenId_);
+    }
+
+    function setApprovalForAll(address operator_, bool approved_) public virtual {
+        _setApprovalForAll(msg.sender, operator_, approved_);
+    }
+
+    // Public Transfer Functions
+
+    function transferFrom(address from_, address to_, uint256 tokenId_) public virtual {
+        require(_isApprovedOrOwner(msg.sender, tokenId_));
+        _transfer(from_, to_, tokenId_);
+    }
+
+    function safeTransferFrom(address from_, address to_, uint256 tokenId_, bytes memory data_) public virtual {
+        transferFrom(from_, to_, tokenId_);
+        //erc721 receivable needed here
+        if (to_.code.length != 0) {
+            (, bytes memory _returned) = to_.staticcall(abi.encodeWithSelector(0x150b7a02, msg.sender, from_, tokenId_, data_));
+            bytes4 _selector = abi.decode(_returned, (bytes4));
+            require(_selector == 0x150b7a02);
+        }
+    }
+
+    function safeTransferFrom(address from_, address to_, uint256 tokenId_) public virtual {
+        safeTransferFrom(from_, to_, tokenId_, "");
+    }
+
+
+    //Custom Multi Transfer Functions
+    function multiTransferFrom(address from_, address to_, uint256[] memory tokenIds_) public virtual {
+        for (uint256 i = 0; i < tokenIds_.length; i++) {
+            transferFrom(from_, to_, tokenIds_[i]);
+        }
+    }
+    function multiSafeTransferFrom(address from_, address to_, uint256[] memory tokenIds_, bytes memory data_) public virtual {
+        for (uint256 i = 0; i < tokenIds_.length; i++) {
+            safeTransferFrom(from_, to_, tokenIds_[i], data_);
+        }
+    }
+
+    function multiTransferToMany(address from_, address[] memory to_, uint256[] memory tokenIds_) public virtual {
+        for (uint256 i = 0; i < tokenIds_.length; i++) {
+            transferFrom(from_, to_[i], tokenids_[i]);
+        }
+    }
+
+    function multiSafeTransferToMany(
+        address from_,
+        address[] memory to_,
+        uint256[] memory tokenIds_,
+        bytes memory data_) public virtual {
+            for (uint256 i = 0; i < tokenIds_.length; i++) {
+                transferFrom(from_, to_[i], tokenids_[i], data_);
+            }
+    }
+
+    //OZ support interface needed here
+    function supportsInterface (bytes4 interfaceId_) public pure returns (bool) {
+        return (interfaceId_ == 0x80ac58cd || interfaceId_ == 0x5b5e139f);
+    }
+
+    //Token URI stuff
+    function tokenURI(uint256 tokenId_) public view virtual returns (string memory) {
+        require(ownerOf[tokenId_] != address(0x0));
+        return string(abi.encodePacked(baseTokenURI, _NumToString(tokenId_), baseTokenExtension));
+    }
+
+    function _setBaseTokenURI(string memory uri_) internal virtual {
+        baseTokenURI = uri_;
+    }
+    function _setBaseTokenURI_Extension(string memory extension_) internal virtual {
+        baseTokenExtension = extension_;
+    }
+
+    //Returns all the tokens owned by the given address
+    function walletOfOwner(address address_) public virtual view returns (uint256[] memory) {
+        uint256 _balance = balanceOf[address_];
+        uint256[] memory _tokenIds = new uint256[] (_balance);
+        uint256 _index;
+        uint256 _supply = totalSupply;
+        for (uint256 i = 0; i < _supply; i++) {
+            if (ownerOf[i] == address(0x0) && _tokenIds[_balance - 1] == 0) {
+                _supply++;
+            }
+            if (ownerOf[i] == address_) {
+                _tokenIds[_index] = i;
+                _index++;
+            }
+        }
+        return _tokenIds;
+    }
+    function tokenOfOwnerByIndex(address address_, uint256 index_) public virtual view returns (uint256) {
+        uint256[] memory _wallet = walletOfOwner(address_);
+        return _wallet[index_];
+    }   
+}
---
+++ FinishedMintContract.sol
@@ -0,0 +1,206 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./ERC721Z.sol";
+
+contract MintContract is ERC721Z {
+
+    uint256 public _amountForMintListSale;
+    uint256 public _amountForAuctionSale;
+    uint256 public _amountForDevMint;
+    uint16 public MaxSupply = 8888;
+    uint8 public MaxMintsPerWallet = 1;
+    uint8 public MaxMintsPerMintlistWallet = 2;
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor() ERC721Z("Name", "Symbol") {
+        _transferOwnership(_msgSender());
+        SaleConfig.isPublicSaleOn = false;
+        SaleConfig.isMintlistSaleOn = false;
+        _amountForAuctionSale = 8000;
+        _amountForMintListSale = 400;
+        _amountForDevMint = 50;
+
+    }
+
+    // sale information
+    struct SaleConfiguration {
+        uint64 _auctionStartTime;
+        uint64 _mintPrice;
+        bool isPublicSaleOn;
+        bool isMintlistSaleOn;
+    }
+
+    SaleConfiguration public SaleConfig;
+
+    // mappings
+    mapping(uint256 => uint256) priceOf;
+    mapping(address => bool) public hasClaimed;
+    mapping(address => bool) public isMintlisted;
+
+    // modifier function pair to check the caller is not a contract
+    function _callerIsUser() internal virtual {
+        require(tx.origin == msg.sender, "Function caller is another contract!");
+    }
+    modifier callerIsUser() {
+        _callerIsUser();
+        _;
+    }
+
+    // OZ Ownerable logic
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    // modifier that calls function saves gas
+    function _onlyOwner() internal virtual {
+        require(owner() == _msgSender(), "Function caller is not the owner");
+    }
+    modifier onlyOwner() {
+        _onlyOwner();
+        _;
+    }
+
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+     
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "New owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+
+
+    // External only owner functions (withdraw/set sale configuration)
+    function setPublicMint(bool _saleOnOrOf) public virtual onlyOwner {
+        SaleConfig.isPublicSaleOn = _saleOnOrOf;
+        SaleConfig._mintPrice = 0.1 ether;
+    }
+    function setAuctionMint(uint64 _startTime) public virtual onlyOwner {
+        SaleConfig._auctionStartTime = _startTime;
+    }
+    function setMintlistMint(bool _saleOnOrOf) public virtual onlyOwner {
+        SaleConfig.isMintlistSaleOn = _saleOnOrOf;
+        SaleConfig._mintPrice = 0.1 ether;
+    }
+
+    // add user to mintlist
+    function MintlistUser(address addr_) external onlyOwner {
+        isMintlisted[addr_] = true;
+    }
+
+    // owner withdraw of funds
+    function withdraw(address to_) external onlyOwner {
+        payable(to_).transfer(address(this).balance);
+    }
+
+
+    uint64 public AuctionStartPrice = 2 ether;
+    uint64 public AuctionPriceDropPerInterval = 0.1 ether;
+    uint64 public AuctionEndPrice = 0.1 ether;
+    uint32 public AuctionLength = 190 minutes;
+    uint16 public AuctionDropInterval = 10 minutes;
+
+    function _getTokenId() internal view returns (uint256) {
+        return totalSupply + 1;
+    }
+    
+    // gets the auction price based on the time
+    function getAuctionMintPrice() internal view virtual returns (uint256) {
+        if (block.timestamp < SaleConfig._auctionStartTime) {
+            return AuctionStartPrice;
+        }
+        if (block.timestamp - SaleConfig._auctionStartTime >= AuctionLength) {
+            return AuctionEndPrice;
+        } else {
+            uint256 NumOfIntervals = uint256((block.timestamp - SaleConfig._auctionStartTime) / AuctionDropInterval);
+            uint256 AuctionPriceAtInterval = AuctionStartPrice - (AuctionPriceDropPerInterval * NumOfIntervals);
+            return AuctionPriceAtInterval;
+        }
+    }
+
+    // auction mint function
+    function AuctionMint(uint32 amount_) external payable callerIsUser {
+        require(block.timestamp > SaleConfig._auctionStartTime, "Auction not started!");
+        require(amount_ == MaxMintsPerWallet, "Invalid mint amount requested!");
+        require(balanceOf[msg.sender] == 0, "Maximum number of NFTs already minted!");
+        require(msg.value == getAuctionMintPrice(), "Invalid amount of ETH!");
+
+        priceOf[_getTokenId()] = msg.value;
+        _mint(msg.sender, _getTokenId());
+    }
+
+    // refunds the difference of a user's bid from the lowest bid in the auction
+    function _refundDifference() external onlyOwner {
+        address[] memory UserAddresses = new address[](auctionSupply);
+        uint256[] memory PricesOfTokens = new uint256[](auctionSupply);
+        uint256 index_;
+        uint256 finalPrice = priceOf[8888];
+        for(uint256 i = 0; i < auctionSupply; i++) {
+            UserAddresses[i] = ownerOf[index_];
+            PricesOfTokens[i] = priceOf[index_];
+            index_++;
+        }
+        for(uint256 i = 0; i < auctionSupply; i++) {
+            payable(UserAddresses[i]).transfer(PricesOfTokens[i] - finalPrice);
+        }
+    }
+
+    // mint list
+    function mintlistMint(uint256 amount_) external payable callerIsUser {
+        require(SaleConfig.isMintlistSaleOn, "Mintlist sale has not started!");
+        require(isMintlisted[msg.sender], "You are not on the mintlist!");
+        require(hasClaimed[msg.sender] != true, "All mints have been claimed!");
+        require((totalSupply + amount_) < (_amountForAuctionSale + _amountForMintListSale + 1),
+        "Max supply has been reached!");
+        require(amount_ < MaxMintsPerMintlistWallet + 1); //as 2 + 1 = 3 and max < 3 = 2
+        uint256[] memory tokens = new uint256[](amount_);
+        for(uint256 i = 0; i < amount_; i++) {
+            tokens[i] = _getTokenId() + i;
+            _mint(msg.sender, tokens[i]);
+        }
+    }
+    
+    // dev mint for team and marketing etc
+    function devMint(uint256 amount_) external onlyOwner {
+        require(totalSupply < MaxSupply, "Max supply has been reached!");
+        require(amountForDevMint % amount_ = 0, "Invalid mint amount!");
+        require(amount_ < amountForDevMint + 1, "Invalid number of mints requested");
+
+        uint256[] tokenIds = new uint256[](amount_);
+        for(uint256 i = 0; i < amount_; i++) {
+            tokenIds[i] = i;
+        }
+        _mint(_owner, tokenIds);
+    }
+
+    /*
+    public mint for the remainder of the nfts
+    */
+    function publicMint(uint256 amount_) external payable callerIsUser {
+        require(SaleConfig.isPublicSaleOn, "Public sale has not started!");
+        require(totalSupply < MaxSupply, "Max supply has been reached!");
+        require(amount_ < MaxMintsPerWallet + 1, "Invalid number of mints requested!");
+        require(msg.value == SaleConfig._mintPrice, "Invalid amount of ETH to mint!");
+
+        _mint(msg.sender, _getTokenId());
+    }
+
+}
